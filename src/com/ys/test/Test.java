86dd549948ecac9bc7aa5145fb30e03e60be90b2
qzyys0130 <qzyys0130@163.com>
2020-09-29 14:45:48 +0800

    test

--- src/com/ys/test/Test.java
+++ src/com/ys/test/Test.java
@@ -2,6 +2,6 @@ package com.ys.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("New World111111");
+        System.out.println("New");
     }
 }
